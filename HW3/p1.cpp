6a7ab07dc4bbb7c36e35babea2926bec77e63fb0
Yuan-Yao Sung <sungyuanyao@gmail.com>
2016-04-07 08:11:34 +0800

    Fix HW3/p1

--- HW3/p1.cpp
+++ HW3/p1.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <iomanip>
+#include <cctype>
 
 using namespace std;
 
 int main()
 {
-    int n;
+    long long int n;
     while(true){
         cin >> n;
         if(cin.eof()){
             break;
-        } else if(cin.fail()){
+        }
+        if(cin.fail() || isalpha(cin.peek())){
             cout << "not a number!" << endl;
             cin.clear();
             cin.ignore(10000, '\n');
         } else {
-            cout << hex << n << ' ' << oct << n << endl; 
+            cout << hex << n << ' ' << oct << n << endl;
         }
     }
     return 0;

d221be5ddaaec9e265621a21d1bc8a03e19dbe09
宋元堯 <deror1869107@gmail.com>
2016-06-04 20:23:31 +0800

    HW7

---
+++ HW7/hw7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class shell {
+	
+};
+
+int main(int argc, char **argv)
+{
+	fstream script;
+	script.open(argv[1]);
+	string line;
+	while(getline(script, line){
+
+	}
+	return 0;
+}
